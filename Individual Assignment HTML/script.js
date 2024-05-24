b68806287a549895ab1dfb157c7c3ba5b3345bdf
aamieyrul <aamieyruljr@gmail.com>
2024-05-24 15:32:37 +0800

    Add files via upload

---
+++ Individual Assignment HTML/script.js
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>My Web Page</title>
+  </head>
+  <body>
+
+    <script>
+      // Your JavaScript code goes here
+      alert("The past was always there, lurking in the corners, 
+      winking at you when you thought you'd moved on, 
+      hanging on to your organs from the inside.");
+    </script>
+  </body>
+</html>
